94d843cc012dcdacb89ee9a9a2e0b79d10871542
Test <test@test.fr>
2026-07-02 12:23:57 +0200

    File cleaning - deleting unused/unchanged models models

--- P2_SAM/Yolo-HLSAM/GPU.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def main():
-    print(torch.__version__)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print( torch.cuda.is_available() )
-if __name__ == '__main__':
-    main()
